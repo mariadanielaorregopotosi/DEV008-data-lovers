5cb191f013196641d8de76ee53fd04a124641a70
maria daniela <orregodaniela8@gmail.com>
2023-06-01 17:08:34 -0500

    no hay cambios

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 // estas funciones son de ejemplo
-
+import data from "./data/ghibli/ghibli.js";
+// import data from './data/rickandmorty/rickandmorty.js';
 export const example = () => {
   return 'example';
 };
