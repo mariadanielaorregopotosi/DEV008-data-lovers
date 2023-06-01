3a6ab0cba2ee5c4cac60aedc7d78d2aeef0ad815
AndreaVazquez22 <kar.andre@icloud.com>
2023-06-01 15:43:22 -0600

    Poster Titulo

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
-// estas funciones son de ejemplo
+// importa data de ghibli.js//
 import ghibli from "./data/ghibli/ghibli.js";
+//se crea const=getData
 export const getData = () => {
- return ghibli.films;
+  return ghibli.films;
 };
 
 export const anotherExample = () => {
-  return 'OMG';
+  return "OMG";
 };
--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
-/* eslint-disable no-console */
 import { getData } from "./data.js";
-const   peliculas = getData();
-console.log (peliculas)
+const peliculas = getData();
+//console.log(peliculas);
 
-function mostrarPeliculas(dataset){
+function mostrarPeliculas(dataset) {
   const contenedor = document.getElementById("peliculas-container");
-  for (let i = 0 ; i < dataset.length; i++) {
+  for (let i = 0; i < dataset.length; i++) {
     const plantilla = `<li><img src = ${peliculas[i].poster}></img><p></p> ${peliculas[i].title} </li>`;
     contenedor.innerHTML += plantilla;
-
-
-  } 
-
   }
- mostrarPeliculas(peliculas);
+}
+mostrarPeliculas(peliculas);
 
-
-const array =["0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
-array.forEach(function() {
+const array = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+];
+array.forEach(function () {
   //console.log("array",array)
-})
+});
